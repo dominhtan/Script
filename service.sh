5ac98a736d7034ade54dfeeff34ffc48458d7549
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-06-28 00:24:41 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -34,8 +34,8 @@ opt1=("Status" "Auto" "Craft")
                 echo "***************************************" 
                 for service in $mysql $php $crond $ssh $network $docker
                 do    
-                      i=`systemctl status $service | grep 'running' && 'loaded' | wc -l`
-                                if [ $i -ge 1 ]
+                      i=`systemctl status $service | grep 'running' | wc -l`
+                        if [ `$i -ge 1` ] 
                         then
                                 echo $service is running
                         else
