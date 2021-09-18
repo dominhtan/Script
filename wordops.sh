005c3f0920f6868225de179ad62a54fbfa17c23b
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-09-19 00:47:28 +0700

    Update wordops.sh

--- wordops.sh
+++ wordops.sh
@@ -20,7 +20,7 @@ wo_update () {
         cp -rf /usr/local/lib/python3.*/dist-packages/usr/* /usr/
         cp -rn /usr/local/lib/python3.*/dist-packages/etc/* /etc/
         cp -f /usr/local/lib/python3.*/dist-packages/etc/bash_completion.d/wo_auto.rc /etc/bash_completion.d/wo_auto.rc
-        wo stack install --quiet --all
+        wo stack install --all
 	wo stack install --php74
         yes | wo stack upgrade
         yes | wo stack migrate --mariadb
