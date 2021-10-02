8cb53057590beedd048d8218a05c462146ebe586
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-10-02 21:01:50 +0700

    Update ssl_secure.sh

--- ssl_secure.sh
+++ ssl_secure.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+printf "Insert domain :"
+read domain
+sudo printf "%s\n" "Insert key: "
+read key
+echo $key > /etc/tls/pki/private/$domain.key
+sudo printf "%s\n" "Insert crt: "
+read crt
+echo $crt > /etc/tls/pki/certs/$domain.crt
 
+#Insert in Proxy
+find /home/proxy/ -type f -name "$domain.conf" | xargs sed -i -e 's/ssl_certificate/#ssl_certificate/g' > $domain.conf
+sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate_key /etc/tls/pki/private/$domain.key;" /home/proxy/$domain.conf
+
+#Insert in Nginx
+find /etc/nginx/conf.d/ -type f -name "$domain_ssl.conf" | xargs sed -i -e 's/ssl_certificate/#ssl_certificate/g' > $domain.ssl.conf
+sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate_key /etc/tls/pki/private/$domain.key;" /etc/nginx/conf.d/$domain_ssl.conf
