979a7edbf667e3e4cc45dc9477d688ab1e8ab62b
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-10-02 21:04:47 +0700

    Update ssl_secure.sh

--- ssl_secure.sh
+++ ssl_secure.sh
@@ -9,9 +9,9 @@ read crt
 echo $crt > /etc/tls/pki/certs/$domain.crt
 
 #Insert in Proxy
-find /home/proxy/ -type f -name "$domain.conf" | xargs sed -i -e 's/ssl_certificate/#ssl_certificate/g' > $domain.conf
-sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate_key /etc/tls/pki/private/$domain.key;" /home/proxy/$domain.conf
+sudo find /home/proxy/ -type f -name "$domain.conf" | xargs sed -i -e 's/ssl_certificate/#ssl_certificate/g' > $domain.conf
+sudo sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate_key /etc/tls/pki/private/$domain.key;" /home/proxy/$domain.conf
 
 #Insert in Nginx
-find /etc/nginx/conf.d/ -type f -name "$domain_ssl.conf" | xargs sed -i -e 's/ssl_certificate/#ssl_certificate/g' > $domain.ssl.conf
-sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate_key /etc/tls/pki/private/$domain.key;" /etc/nginx/conf.d/$domain_ssl.conf
+sudo find /etc/nginx/conf.d/ -type f -name "$domain_ssl.conf" | xargs sed -i -e 's/ssl_certificate/#ssl_certificate/g' > $domain.ssl.conf
+sudo sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate_key /etc/tls/pki/private/$domain.key;" /etc/nginx/conf.d/$domain_ssl.conf
