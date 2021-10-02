9cc1e8e674c9cd00759220d18a272acd351cf58d
dominhtan <85058029+dominhtan@users.noreply.github.com>
2021-10-02 21:09:20 +0700

    Update ssl_secure.sh

--- ssl_secure.sh
+++ ssl_secure.sh
@@ -15,3 +15,7 @@ sudo sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate
 #Insert in Nginx
 sudo find /etc/nginx/conf.d/ -type f -name "$domain_ssl.conf" | xargs sed -i -e 's/ssl_certificate/#ssl_certificate/g' > $domain.ssl.conf
 sudo sed -i "4 i ssl_certificate /etc/tls/pki/certs/$domain.crt; ssl_certificate_key /etc/tls/pki/private/$domain.key;" /etc/nginx/conf.d/$domain_ssl.conf
+
+#Reload Nginx
+sudo nginx -t
+sudo service nginx reload
