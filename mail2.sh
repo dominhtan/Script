5c4d41a85b8ee216804ee220ddd334f3042d51bc
dominhtan <85058029+dominhtan@users.noreply.github.com>
2022-08-23 14:47:12 +0700

    Update mail2.sh

--- mail2.sh
+++ mail2.sh
@@ -4,3 +4,4 @@
 imapsync --host1 115.146.127.14 --user1 $u1 --password1 $p1 --ssl1 --host2 52.98.66.98 --user2 $u2 --password2 $p2 --ssl2 --syncinternaldates
 done; } < users.txt
 
+// example users.txt: user001_1;password001_1;user001_2;password001_2
