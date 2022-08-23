ca496a5ac627b82622b566c34de9a08a0e5e8072
dominhtan <85058029+dominhtan@users.noreply.github.com>
2022-08-23 14:45:56 +0700

    Create mail2.sh

---
+++ mail2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+{ while IFS=';' read  u1 p1 u2 p2; do
+imapsync --host1 115.146.127.14 --user1 $u1 --password1 $p1 --ssl1 --host2 52.98.66.98 --user2 $u2 --password2 $p2 --ssl2 --syncinternaldates
+done; } < users.txt
+
