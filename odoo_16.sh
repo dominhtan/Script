931cbafc7435251bd4da7eac8456538336fdb2e4
Umemura Kanzaki <85058029+dominhtan@users.noreply.github.com>
2023-07-27 09:57:47 +0700

    Update odoo_16.sh

--- odoo_16.sh
+++ odoo_16.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 sudo adduser --system --quiet --shell=/bin/bash --home=/opt/odoo --gecos 'odoo' --group odoo
 sudo mkdir /etc/odoo && mkdir /var/log/odoo/
-sudo apt-get update && apt-get install postgresql postgresql-server-dev-12 build-essential python3-pillow python3-lxml python3-dev python3-pip python3-setuptools npm nodejs git gdebi libldap2-dev libsasl2-dev  libxml2-dev libxslt1-dev libjpeg-dev -y
+sudo apt-get update && sudo apt-get install postgresql postgresql-server-dev-all python3-pil python3-lxml python3-dev python3-pip python3-setuptoolsnpm nodejs git gdebi libldap2-dev libsasl2-dev libxml2-dev libxslt1-dev libjpeg-dev python3-pip python3 zlib1g-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev gdebi-core -y
+
 sudo service postgresql restart
 git clone --depth=1 --branch=16.0 https://github.com/odoo/odoo.git /opt/odoo/odoo
 sudo chown odoo:odoo /opt/odoo/ -R && sudo chown odoo:odoo /var/log/odoo/ -R && cd /opt/odoo/odoo && sudo pip3 install -r requirements.txt
@@ -16,7 +16,6 @@ sudo sed -i "s,^\(logfile = \).*,\1"/var/log/odoo/odoo-server.log"," /etc/odoo/o
 sudo sed -i "s,^\(logrotate = \).*,\1"True"," /etc/odoo/odoo.conf
 sudo sed -i "s,^\(proxy_mode = \).*,\1"True"," /etc/odoo/odoo.conf
 sudo cp /opt/odoo/odoo/debian/init /etc/init.d/odoo && chmod +x /etc/init.d/odoo
-sudo ln -s /opt/odoo/odoo/odoo-bin /usr/bin/odoo
 sudo update-rc.d -f odoo start 20 2 3 4 5 .
 sudo service odoo restart
 ufw disable
